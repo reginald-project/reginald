8f26540053715812ea521d77a65b7c2f64545bd7
Antti Kivi <antti@anttikivi.fi>
2025-05-11 14:33:25 +0300

    feat: add a bootstrap logger

--- cmd/reginald/main.go
+++ cmd/reginald/main.go
@@ -5,14 +5,24 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/anttikivi/reginald/internal/cli"
 	"github.com/anttikivi/reginald/internal/cli/root"
+	"github.com/anttikivi/reginald/internal/logging"
 	"github.com/anttikivi/reginald/internal/version"
 )
 
 func main() {
+	if err := logging.InitBootstrap(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	slog.Debug("bootstrap logger initialized")
+	slog.Info("bootstrapping Reginald", "version", version.Version)
+
 	rootCmd := root.New(version.Version)
 
 	if err := cli.Run(rootCmd); err != nil {
---
+++ internal/logging/logging.go
@@ -0,0 +1,54 @@
+// Package logging defines utilities for logging within Reginald. The program
+// uses the [log/slog] package for logging, and this package contains the
+// function for setting up the logging.
+//
+// At the first phase before parsing the configuration, logging is done using
+// the bootstrap logger that is set as the default logger first. After the
+// bootstrapping, the default logger should be replaced with the actual logger
+// that is set up according to the user's configuration. The bootstrap logger is
+// configured using environment variables if customizing it is needed.
+package logging
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"strings"
+)
+
+// Errors for logging.
+var (
+	errInvalidOutput = errors.New("given log output not supported")
+)
+
+// InitBootstrap initializes the bootstrap logger and sets it as the default
+// logger in [log/slog]. It returns the encountered error.
+func InitBootstrap() error {
+	outputName := os.Getenv("REGINALD_BOOTSTRAP_OUTPUT")
+	if outputName == "" {
+		// TODO: The default should be file, but let's implement that later.
+		outputName = "stderr"
+	}
+
+	var output io.Writer
+
+	switch strings.ToLower(outputName) {
+	case "stderr":
+		output = os.Stderr
+	case "stdout":
+		output = os.Stdout
+	default:
+		return fmt.Errorf("failed to create the bootstrap logger: %w", errInvalidOutput)
+	}
+
+	handler := slog.NewTextHandler(output, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+	logger := slog.New(handler)
+
+	slog.SetDefault(logger)
+
+	return nil
+}
