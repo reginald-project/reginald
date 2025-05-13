582cac89d5747593ef6e3a11e96fcc2e4ca71a1b
Antti Kivi <antti@anttikivi.fi>
2025-05-13 15:37:59 +0300

    feat: add pathname utilities

---
+++ internal/pathname/pathname.go
@@ -0,0 +1,101 @@
+// Package pathname contains generic filepath-related utilities that complement
+// the standard library [path/filepath].
+package pathname
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+)
+
+// Abs returns an absolute representation of path. Relative paths will be joined
+// with the current working directory. Abs calls Clean on the result. Abs also
+// resolves user home directories and environment variables.
+func Abs(path string) (string, error) {
+	path = ExpandEnv(path)
+
+	var err error
+
+	path, err = ExpandUser(path)
+	if err != nil {
+		return "", fmt.Errorf("failed to expand user home directory: %w", err)
+	}
+
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return "", fmt.Errorf("%w", err)
+	}
+
+	return path, nil
+}
+
+// ExpandEnv replaces ${var} or $var and even %var% on Windows in the string
+// according to the values of the current environment variables. References to
+// undefined variables are replaced by an empty string.
+func ExpandEnv(path string) string {
+	return expandOSEnv(path)
+}
+
+// ExpandUser tries to replace "~" or "~username" in the string to match the
+// correspending user's home directory. If the wanted user does not exist, this
+// function returns an error.
+func ExpandUser(path string) (string, error) {
+	if path == "~" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("failed to get the user home dir: %w", err)
+		}
+
+		return home, nil
+	}
+
+	if strings.HasPrefix(path, "~") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("failed to get the user home dir: %w", err)
+		}
+
+		// Using the current user's home directory.
+		if path[1] == '/' || path[1] == os.PathSeparator {
+			return filepath.Join(home, path[1:]), nil
+		}
+
+		path, err = expandOtherUser(path)
+		if err != nil {
+			return "", fmt.Errorf("%w", err)
+		}
+	}
+
+	return path, nil
+}
+
+// expandOtherUser tries to replace "~username" in path to match the
+// correspending user's home directory. If the wanted user does not exist, this
+// function returns an error.
+func expandOtherUser(path string) (string, error) {
+	var (
+		i        int
+		username string
+	)
+
+	if i = strings.IndexByte(path, os.PathSeparator); i != -1 {
+		username = path[1:i]
+	} else if i = strings.IndexByte(path, '/'); i != -1 {
+		username = path[1:i]
+	} else {
+		username = path[1:]
+	}
+
+	u, err := user.Lookup(username)
+	if err != nil {
+		return "", fmt.Errorf("failed to look up user %q: %w", username, err)
+	}
+
+	if i == -1 {
+		return u.HomeDir, nil
+	}
+
+	return filepath.Join(u.HomeDir, path[i:]), nil
+}
---
+++ internal/pathname/pathname_windows.go
@@ -0,0 +1,63 @@
+//go:build windows
+
+package pathname
+
+import (
+	"os"
+	"strings"
+)
+
+// expandOSEnv replaces %var%, ${var}, or $var in the string according to the
+// values of the current environment variables. References to undefined
+// variables are replaced by the empty string.
+func expandOSEnv(path string) string {
+	if strings.Contains(path, "%") {
+		path = expandWinEnv(path)
+	}
+
+	path = os.ExpandEnv(path)
+
+	return path
+}
+
+func expandWinEnv(s string) string {
+	var buf []byte
+	// %% is all ASCII, so bytes are fine for this operation.
+	i := 0
+	for j := 0; j < len(s); j++ {
+		if s[j] == '%' && j+1 < len(s) {
+			if buf == nil {
+				buf = make([]byte, 0, 2*len(s))
+			}
+
+			buf = append(buf, s[i:j]...)
+
+			var k int
+			for k = j + 1; k < len(s) && isAlphaNum(s[k]); k++ {
+			}
+
+			if k == j+1 {
+				buf = append(buf, '%')
+			} else if s[k] == '%' {
+				buf = append(buf, os.Getenv(s[j+1:k])...)
+			} else {
+				buf = append(buf, s[j:k+1]...)
+			}
+
+			j = k + 1
+			i = j
+		}
+	}
+
+	if buf == nil {
+		return s
+	}
+
+	return string(buf) + s[i:]
+}
+
+// isAlphaNum reports whether the byte is an ASCII letter, number, or
+// underscore.
+func isAlphaNum(c byte) bool {
+	return c == '_' || '0' <= c && c <= '9' || 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z'
+}
