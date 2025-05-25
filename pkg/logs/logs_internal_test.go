34c285ee5cbd6d1b9f0595bd39ff740ccad75663
Antti Kivi <antti@anttikivi.fi>
2025-05-25 22:40:22 +0300

    test: add tests for the logging utilities

---
+++ pkg/logs/logs_internal_test.go
@@ -0,0 +1,153 @@
+package logs
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestLevelString(t *testing.T) {
+	t.Parallel()
+
+	for _, test := range []struct {
+		in   Level
+		want string
+	}{
+		{0, "INFO"},
+		{LevelError, "ERROR"},
+		{LevelError + 2, "ERROR+2"},
+		{LevelError - 2, "WARN+2"},
+		{LevelWarn, "WARN"},
+		{LevelWarn - 1, "INFO+3"},
+		{LevelInfo, "INFO"},
+		{LevelInfo + 1, "INFO+1"},
+		{LevelInfo - 3, "DEBUG+1"},
+		{LevelDebug, "DEBUG"},
+		{LevelDebug - 2, "TRACE+2"},
+		{LevelDebug - 3, "TRACE+1"},
+		{LevelTrace, "TRACE"},
+		{LevelTrace - 2, "TRACE-2"},
+	} {
+		got := test.in.String()
+		if got != test.want {
+			t.Errorf("%d: got %s, want %s", test.in, got, test.want)
+		}
+	}
+}
+
+func TestLevelMarshalJSON(t *testing.T) {
+	t.Parallel()
+
+	want := LevelWarn - 3
+	wantData := []byte(`"INFO+1"`)
+
+	data, err := want.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(data, wantData) {
+		t.Errorf("got %s, want %s", string(data), string(wantData))
+	}
+
+	var got Level
+	if err := got.UnmarshalJSON(data); err != nil {
+		t.Fatal(err)
+	}
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestLevelMarshalText(t *testing.T) {
+	t.Parallel()
+
+	want := LevelWarn - 3
+	wantData := []byte("INFO+1")
+
+	data, err := want.MarshalText()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(data, wantData) {
+		t.Errorf("got %s, want %s", string(data), string(wantData))
+	}
+
+	var got Level
+	if err := got.UnmarshalText(data); err != nil {
+		t.Fatal(err)
+	}
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func TestLevelAppendText(t *testing.T) {
+	t.Parallel()
+
+	buf := make([]byte, 4, 16)
+	want := LevelWarn - 3
+	wantData := []byte("\x00\x00\x00\x00INFO+1")
+
+	data, err := want.AppendText(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(data, wantData) {
+		t.Errorf("got %s, want %s", string(data), string(wantData))
+	}
+}
+
+func TestLevelParse(t *testing.T) {
+	t.Parallel()
+
+	for _, test := range []struct {
+		in   string
+		want Level
+	}{
+		{"DEBUG", LevelDebug},
+		{"INFO", LevelInfo},
+		{"WARN", LevelWarn},
+		{"ERROR", LevelError},
+		{"debug", LevelDebug},
+		{"iNfo", LevelInfo},
+		{"INFO+87", LevelInfo + 87},
+		{"Error-18", LevelError - 18},
+		{"Error-8", LevelInfo},
+	} {
+		var got Level
+		if err := got.parse(test.in); err != nil {
+			t.Fatalf("%q: %v", test.in, err)
+		}
+
+		if got != test.want {
+			t.Errorf("%q: got %s, want %s", test.in, got, test.want)
+		}
+	}
+}
+
+func TestLevelParseError(t *testing.T) {
+	t.Parallel()
+
+	for _, test := range []struct {
+		in   string
+		want string // error string should contain this
+	}{
+		{"", "unknown name"},
+		{"dbg", "unknown name"},
+		{"INFO+", "invalid syntax"},
+		{"INFO-", "invalid syntax"},
+		{"ERROR+23x", "invalid syntax"},
+	} {
+		var l Level
+
+		err := l.parse(test.in)
+		if err == nil || !strings.Contains(err.Error(), test.want) {
+			t.Errorf("%q: got %v, want string containing %q", test.in, err, test.want)
+		}
+	}
+}
