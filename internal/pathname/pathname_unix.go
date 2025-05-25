53d3bf14a4356b7afd1437ffc1ffc02c98ec10b0
Antti Kivi <antti@anttikivi.fi>
2025-05-25 23:02:39 +0300

    feat: add a Path type

--- internal/pathname/pathname_unix.go
+++ internal/pathname/pathname_unix.go
@@ -7,6 +7,6 @@ import "os"
 // expandOSEnv replaces ${var} or $var in the string according to the values of
 // the current environment variables. References to undefined variables are
 // replaced by empty string.
-func expandOSEnv(path string) string {
-	return os.ExpandEnv(path)
+func expandOSEnv(path Path) Path {
+	return Path(os.ExpandEnv(string(path)))
 }
