243a38562cab02a5405c2c5c7b3761e6e91ebafd
Antti Kivi <antti@anttikivi.fi>
2025-07-01 23:16:17 +0300

    feat: add task sorting

--- internal/plugin/task.go
+++ internal/plugin/task.go
@@ -15,11 +15,26 @@
 package plugin
 
 import (
+	"errors"
+	"fmt"
 	"log/slog"
 	"strings"
 
 	"github.com/reginald-project/reginald-sdk-go/api"
 	"github.com/reginald-project/reginald/internal/system"
+)
+
+// Constants for the node visit statuses when traversing TaskGraph.
+const (
+	unvisited visitState = iota
+	visiting
+	visited
+)
+
+// Errors returned by the graph functions.
+var (
+	errCycle = errors.New("circular task dependencies detected")
+	errNilID = errors.New("task config with empty ID")
 )
 
 // A Task is the program representation of a plugin task type that is defined in
@@ -55,11 +70,122 @@ type TaskConfig struct {
 // TaskDefaults is the type for the default config values set for the tasks.
 type TaskDefaults map[string]map[string]any
 
+// TaskGraph is a graph of TaskNodes that can be sorted topographically
+// to determine the execution order of the task instances.
+type TaskGraph map[string]*TaskNode
+
+// A TaskNode is a node in the task graph that determines the execution order of
+// the tasks.
+type TaskNode struct {
+	ID           string      // ID of the task in question
+	Type         string      // type of the task in question
+	Dependencies []string    // dependencies of the task in question
+	Dependents   []*TaskNode // nodes for the tasks that are dependent on this task
+	DegreeIn     int         // number of incoming edges
+}
+
 // TaskRequirements is list of tasks a task depends on.
 type TaskRequirements []string
 
 // logTasks is a helper type for logging a slice of tasks.
 type logTasks []*Task
+
+// visitState is the type for the visit indicator during the cycle detection in
+// TaskGraph.
+type visitState int
+
+// NewTaskGraph returns a new TaskGraph built from the given task configuration.
+func NewTaskGraph(cfgs []TaskConfig) (TaskGraph, error) {
+	graph := make(TaskGraph)
+
+	for _, cfg := range cfgs {
+		if cfg.ID == "" {
+			// TODO: Automatically add the missing tasks if a dependency is just
+			// a task type. This should be done earlier and not here, but this
+			// warrants a panic once this is implemented.
+			// panic(fmt.Sprintf("%v: task of type %s", errNilID, cfg.Type))
+			return nil, fmt.Errorf("%w: task of type %s", errNilID, cfg.Type)
+		}
+
+		graph[cfg.ID] = &TaskNode{
+			ID:           cfg.ID,
+			Type:         cfg.Type,
+			Dependencies: cfg.Requires, // dependencies should be normalized before this
+			Dependents:   make([]*TaskNode, 0),
+			DegreeIn:     0,
+		}
+	}
+
+	for _, node := range graph {
+		for _, d := range node.Dependencies {
+			depNode, ok := graph[d]
+			if !ok {
+				panic(
+					fmt.Sprintf(
+						"task %q (type %s) has unknown dependency: %s",
+						node.ID,
+						node.Type,
+						d,
+					),
+				)
+			}
+
+			depNode.Dependents = append(depNode.Dependents, node)
+			node.DegreeIn++
+		}
+	}
+
+	if err := graph.checkCycles(); err != nil {
+		return nil, err
+	}
+
+	return graph, nil
+}
+
+// Sorted returns g as a topologically sorted list of stages for running. Each
+// element of the slice is a slice that contains the tasks that can be executed
+// in parallel.
+func (g TaskGraph) Sorted() ([][]*TaskNode, error) {
+	queue := make([]*TaskNode, 0)
+
+	for _, node := range g {
+		if node.DegreeIn == 0 {
+			queue = append(queue, node)
+		}
+	}
+
+	var ( //nolint:prealloc // no need to preallocate here
+		stages [][]*TaskNode
+		sorted []*TaskNode
+	)
+
+	for len(queue) > 0 {
+		current := make([]*TaskNode, len(queue))
+
+		copy(current, queue)
+
+		stages = append(stages, current)
+
+		queue = nil
+
+		for _, node := range current {
+			sorted = append(sorted, node)
+
+			for _, dependent := range node.Dependents {
+				dependent.DegreeIn--
+				if dependent.DegreeIn == 0 {
+					queue = append(queue, dependent)
+				}
+			}
+		}
+	}
+
+	if len(sorted) != len(g) {
+		return nil, errCycle
+	}
+
+	return stages, nil
+}
 
 // UnmarshalText implements [encoding.TextUnmarshaler]. It decodes a single
 // string into TaskRequirements.
@@ -105,6 +231,48 @@ func (t *Task) LogValue() slog.Value {
 	return slog.GroupValue(slog.String("type", t.Type), slog.String("description", t.Description))
 }
 
+// checkCycles checks if g contains cycles and returns an error if it does.
+func (g TaskGraph) checkCycles() error {
+	state := make(map[string]visitState, len(g))
+
+	for id := range g {
+		state[id] = unvisited
+	}
+
+	for id, node := range g {
+		if state[id] == unvisited {
+			stack := make([]*TaskNode, 0)
+			if err := visit(node, state, &stack); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// newCycleError formats and returns an error for circular dependencies.
+func newCycleError(startNode *TaskNode, stack []*TaskNode) error {
+	path := ""
+	startIndex := -1
+
+	for i, node := range stack {
+		if node.ID == startNode.ID {
+			startIndex = i
+
+			break
+		}
+	}
+
+	for i := startIndex; i < len(stack); i++ {
+		path += stack[i].ID + " -> "
+	}
+
+	path += startNode.ID
+
+	return fmt.Errorf("%w: %s", errCycle, path)
+}
+
 // newTasks creates the internal task representations for the given plugin. It
 // panics if the plugin is nil.
 func newTasks(plugin Plugin) []*Task {
@@ -127,4 +295,35 @@ func newTasks(plugin Plugin) []*Task {
 	}
 
 	return tasks
+}
+
+func visit(node *TaskNode, state map[string]visitState, stack *[]*TaskNode) error {
+	state[node.ID] = visiting
+
+	*stack = append(*stack, node)
+
+	for _, dependent := range node.Dependents {
+		switch state[dependent.ID] {
+		case unvisited:
+			if err := visit(dependent, state, stack); err != nil {
+				return err
+			}
+		case visiting:
+			return newCycleError(dependent, *stack)
+		case visited:
+			continue
+		default:
+			panic(
+				fmt.Sprintf(
+					"invalid state while visiting task graph node: %v",
+					state[dependent.ID],
+				),
+			)
+		}
+	}
+
+	state[node.ID] = visited
+	*stack = (*stack)[:len(*stack)-1]
+
+	return nil
 }
